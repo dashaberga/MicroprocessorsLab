f63a9aa5a335131295b9095509bc26a47e540c3a
Ml5716 <Ml5716@skbl-422-004.ph.ic.ac.uk>
2019-01-14 15:47:02 +0000

    Fixed delay time reload after first cycle, added cascading delay

--- Simple1.asm
+++ Simple1.asm
@@ -10,10 +10,10 @@ start
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
-loop	call    delay, 0
-	movlw   0x13
-	movwf   0x05
-	movff 	0x06, PORTC
+loop	movlw   0x13		    
+	movwf   0x05		    ; Reset first delay counter
+	call    delay, 0	    ; Call delay loop
+	movff 	0x06, PORTC	    ; Output value to Port C
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
 	movf    PORTD, W
@@ -21,13 +21,13 @@ test	movwf	0x06, ACCESS	    ; Test for end of loop condition
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 delay	movlw   0x13
-	movwf   0x04
-	call delay2
-	decfsz  0x05
-	bra     delay
-	return
-delay2  decfsz  0x04
-	bra delay2
-	return
+	movwf   0x04		    ; Reset 2nd delay counter
+	call delay2		    ; Call second nested delay loop
+	decfsz  0x05		    ; Decremant counter, skip if zero
+	bra     delay		    ; Loop back to delay
+	return			    ; Return to loop
+delay2  decfsz  0x04		    ; Decremant counter, skip if zero
+	bra     delay2		    ; Loop back to delay2
+	return			    ; Return to delay loop
 	
 	end
