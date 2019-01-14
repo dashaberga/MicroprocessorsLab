f15085b1d34ef6a2c3bf689be27088db18ed395e
Ml5716 <Ml5716@skbl-422-004.ph.ic.ac.uk>
2019-01-14 14:24:08 +0000

    Added delay to loop increment

--- Simple1.asm
+++ Simple1.asm
@@ -7,15 +7,21 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw   0x13
+	movwf   0x05
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
-loop	movff 	0x06, PORTC
+loop	call    delay, 0
+	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
 	movf    PORTD, W
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+delay	decfsz  0x05
+	bra     delay
+	return
 	
 	end
