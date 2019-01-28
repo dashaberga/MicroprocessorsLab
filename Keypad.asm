c95d1e23dffb7158004d6e74ec729cbe4dcff617
Ml5716 <Ml5716@skbl-422-004.ph.ic.ac.uk>
2019-01-28 14:43:42 +0000

    Keypad code loops twice & works, but need to store values

--- Keypad.asm
+++ Keypad.asm
@@ -5,22 +5,85 @@
     Keypad code
     
 Press_test
+    movlw    0x02
+    movwf    0x0B    
     movlw    0x00
     movwf    PORTE
     banksel  PADCFG1 
     bsf      PADCFG1, REPU, BANKED
     movlb    0x00
+Press_test_1    
     movlw    0x0F    ;sets 0-3 to output and 4-7 to input
     movwf    TRISE
     movff    PORTE, WREG 
-    movwf    0x06
+    movwf    0x08
+    movwf    0x09
+    
+    movlw    0x0F
+    subwf    0x08
+    bz    no_press
+    movff    0x09, 0x08
+    
+    movlw    0x0E
+    subwf    0x08
+    bz    button_select
+    movff    0x09, 0x08
+    
+    movlw    0x0D
+    subwf    0x08
+    bz    button_select
+    movff    0x09, 0x08
+    
+    movlw    0x0B
+    subwf    0x08
+    bz    button_select
+    movff    0x09, 0x08
+    
+    movlw    0x07
+    subwf    0x08
+    bz    button_select
+    movff    0x09, 0x08
+    
+    bra no_press
+    
+Press_test_2
     movlw    0xF0    ;sets 4-7 to output and 0-3 to input
     movwf    TRISE
     movff    PORTE, WREG
-    movff    PORTE, WREG 
-    movwf    0x06
+    movwf    0x08
+    movwf    0x09
     
-    return
+    movlw    0xE0
+    subwf    0x08
+    bz    button_select2
+    movff    0x09, 0x08
+    
+    movlw    0xD0
+    subwf    0x08
+    bz    button_select2
+    movff    0x09, 0x08
+    
+    movlw    0xB0
+    subwf    0x08
+    bz    button_select2
+    movff    0x09, 0x08
+    
+    movlw    0x70
+    subwf    0x08
+    bz    button_select2
+    movff    0x09, 0x08
+    
+    bra no_press
+        
+button_select
+    movff    0x09, 0x0A
+    bra Press_test_2
+    
+button_select2
+    movf    0x09, W
+    addwf   0x0A
+    decfsz  0x0B
+    bra Press_test_1
     
     
     
@@ -29,8 +92,11 @@ Press_test
     
     
     
+
+press    
     
-    
+no_press    
+    return    
     
     
     
--- Simple1.asm
+++ Simple1.asm
@@ -1,7 +1,8 @@
 	#include p18f87k22.inc
 
 	extern	UART_Setup, UART_Transmit_Message  ; external UART subroutines
-	extern  LCD_Setup, LCD_Write_Message, LCD_clear, Line_set_2	    ; external LCD subroutines
+	extern  LCD_Setup, LCD_Write_Message, LCD_clear, Line_set_2 ; external LCD subroutines
+	extern  Press_test
 	
 acs0	udata_acs   ; reserve data space in access ram
 counter	    res 1   ; reserve one byte for a counter variable
@@ -29,7 +30,9 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	goto	start
 	
 	; ******* Main programme ****************************************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
+start 	call    Press_test
+	
+	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
 	movlw	upper(myTable)	; address of data in PM
 	movwf	TBLPTRU		; load upper bits to TBLPTRU
 	movlw	high(myTable)	; address of data in PM
