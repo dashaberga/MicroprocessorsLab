d46b18a372fd5e7855c1eb3e07484a5a2787a834
Ml5716 <Ml5716@skbl-422-004.ph.ic.ac.uk>
2019-01-28 11:25:01 +0000

    Keypad code reads value, but simple1 not finished

--- Keypad.asm
+++ Keypad.asm
@@ -1,3 +1,41 @@
-
+#include p18f87k22.inc
+   
+    global Press_test
+    
+    Keypad code
+    
+Press_test
+    movlw    0x00
+    movwf    PORTE
+    banksel  PADCFG1 
+    bsf      PADCFG1, REPU, BANKED
+    movlb    0x00
+    movlw    0x0F    ;sets 0-3 to output and 4-7 to input
+    movwf    TRISE
+    movff    PORTE, WREG 
+    movwf    0x06
+    movlw    0xF0    ;sets 4-7 to output and 0-3 to input
+    movwf    TRISE
+    movff    PORTE, WREG
+    movff    PORTE, WREG 
+    movwf    0x06
+    
+    return
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+end
 
 
