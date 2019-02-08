fcf4874e71ad22e8268ad043efcfbc0679e5ce4f
ml5716 <ml5716@skbl-422-004.ph.ic.ac.uk>
2019-02-08 11:47:22 +0000

    Reverted back to using timer 0 (still 1.04 second turnover)

--- DAC.asm
+++ DAC.asm
@@ -10,18 +10,18 @@ temp_storage res 1
 
 int_hi code 0x0008 ; high vector, no low vector
     
-    btfss INTCON,TMR1IF ; check that this is timer0 interrupt 
+    btfss INTCON,TMR0IF ; check that this is timer0 interrupt 
     retfie FAST ; if not then return
     movlw  0xdc
-    movwf  TMR1L
+    movwf  TMR0L
     movlw  0x0b
-    movlw  TMR1H 
+    movlw  TMR0H 
     movff time_sec, temp_storage
     movlw 0x3b
     subwf temp_storage
     bz inc_minute
     incf time_sec
-    bcf INTCON,TMR1IF ; clear interrupt flag
+    bcf INTCON,TMR0IF ; clear interrupt flag
     retfie FAST ; fast return from interrupt
 inc_minute
     movlw 0x00
@@ -31,7 +31,7 @@ inc_minute
     subwf temp_storage
     bz inc_hour
     incf time_min
-    bcf INTCON,TMR1IF ; clear interrupt flag
+    bcf INTCON,TMR0IF ; clear interrupt flag
     retfie FAST ; fast return from interrupt
 inc_hour
     movlw 0x00
@@ -41,22 +41,22 @@ inc_hour
     subwf temp_storage
     bz new_day
     incf time_hour
-    bcf INTCON,TMR1IF ; clear interrupt flag
+    bcf INTCON,TMR0IF ; clear interrupt flag
     retfie FAST ; fast return from interrupt
 new_day
     movlw 0x00
     movwf time_sec
     movwf time_min
     movwf time_hour
-    bcf INTCON,TMR1IF ; clear interrupt flag
+    bcf INTCON,TMR0IF ; clear interrupt flag
     retfie FAST ; fast return from interrupt
     
 DAC code
 
 DAC_Setup
     movlw b'10000111' ; Set timer0 to 16-bit, Fosc/4/256 
-    movwf T1CON ; = 62.5KHz clock rate, approx 1sec rollover
-    bsf INTCON,TMR1IE ; Enable timer0 interrupt 
+    movwf T0CON ; = 62.5KHz clock rate, approx 1sec rollover
+    bsf INTCON,TMR0IE ; Enable timer0 interrupt 
     bsf INTCON,GIE ; Enable all interrupts return
     movlw 0x30
     movwf time_sec
