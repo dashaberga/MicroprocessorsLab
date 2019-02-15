75634710a377b7b8971e1169f69797400a206e18
ml5716 <ml5716@skbl-422-004.ph.ic.ac.uk>
2019-02-15 11:59:19 +0000

    Displays time, day of week, and date. Can set time and weekday but not date

--- Keypad.asm
+++ Keypad.asm
@@ -3,7 +3,8 @@
     
     global Press_test, Keypad_Setup
     extern LCD_Write_Message, Line_set_2, Line_set_1
-    extern mode_counter
+    extern mode_counter, write_date, write_time
+    extern time_sec, time_min, time_hour, time_day, time_week, time_month, time_year
     
  
 acs0	udata_acs   ; reserve data space in access ram
@@ -17,6 +18,7 @@ cnt_h    res 1   ; reserve 1 byte for variable cnt_h
 cnt_ms   res 1   ; reserve 1 byte for ms counter
 position res 1   ; reserve 1 byte for tracking position on screen.
 		  ; lowest 4 bits are x, next bit is y
+set_position res 1
    
 Keypad code
     
@@ -161,82 +163,117 @@ translate        ;multiple if statements for every valid command
 	movff output1, output2
 	movlw 0xEE
 	subwf output2
-	bz character1
+	bz character10
 	
 	movff output1, output2
 	movlw 0xED
 	subwf output2
-	bz character2
+	bz character20
 	
 	movff output1, output2
 	movlw 0xEB
 	subwf output2
-	bz character3
+	bz character30
 	
 	movff output1, output2
 	movlw 0xDE
 	subwf output2
-	bz character4
+	bz character40
 	
 	movff output1, output2
 	movlw 0xDD
 	subwf output2
-	bz character5
+	bz character50
 	
 	movff output1, output2
 	movlw 0xDB
 	subwf output2
-	bz character6
+	bz character60
 	
 	movff output1, output2
 	movlw 0xBE
 	subwf output2
-	bz character7
+	bz character70
 	
 	movff output1, output2
 	movlw 0xBD
 	subwf output2
-	bz character8
+	bz character80
 	
 	movff output1, output2
 	movlw 0xBB
 	subwf output2
-	bz character9
+	bz character90
 	
 	movff output1, output2
 	movlw 0x7D
 	subwf output2
-	bz character0
+	bz character00
 	
 	movff output1, output2
 	movlw 0x7E
 	subwf output2
-	bz characterA
+	bz characterA0
 	
 	movff output1, output2
 	movlw 0x7B
 	subwf output2
-	bz characterB
+	bz characterB0
 	
 	movff output1, output2
 	movlw 0x77
 	subwf output2
-	bz characterC
+	bz characterC0
 	
 	movff output1, output2
 	movlw 0xB7
 	subwf output2
-	bz characterD
+	bz characterD0
 	
 	movff output1, output2
 	movlw 0xD7
 	subwf output2
-	bz characterE
+	bz characterE0
 	
 	movff output1, output2
 	movlw 0xE7
 	subwf output2
-	bz characterF
+	bz characterF0
+
+character10
+	goto character1
+character20
+	goto character2
+character30
+	goto character3
+character40
+	goto character4
+character50
+	goto character5
+character60
+	goto character6
+character70
+	goto character7
+character80
+	goto character8
+character90
+	goto character9
+character00
+	goto character0
+characterA0
+	goto characterA 
+characterB0
+	goto characterB
+characterC0
+	goto characterC
+characterD0
+	goto characterD
+characterE0
+	goto characterE
+characterF0
+	goto characterF
+	
+	
 	
 character1	;print the ascii character onto the screen
 	movlw   0x31
@@ -247,12 +284,64 @@ character1	;print the ascii character onto the screen
 	bra     release
 	
 character2	
-	movlw   0x32
-	movwf   output2
-	movlw	1	; output message to LCD
-	lfsr	FSR2, output2
-	call	LCD_Write_Message
-	bra     release
+	btfsc   mode_counter, 0
+	bra	up
+	bra	other
+up
+	movff set_position, output1
+	movlw 0x00
+	subwf output1
+	bz hour_up
+	
+	movff set_position, output1
+	movlw 0x01
+	subwf output1
+	bz minute_up
+	
+	movff set_position, output1
+	movlw 0x02
+	subwf output1
+	bz second_up
+	
+	movff set_position, output1
+	movlw 0x03
+	subwf output1
+	bz week_up
+	
+hour_up
+	movff time_hour, output1
+	movlw 0x17
+	subwf output1
+	bz no_increase
+	incf time_hour
+	call write_time
+	goto release
+minute_up
+	movff time_min, output1
+	movlw 0x3b
+	subwf output1
+	bz no_increase
+	incf time_min
+	call write_time
+	goto release
+second_up
+	movff time_sec, output1
+	movlw 0x3b
+	subwf output1
+	bz no_increase
+	incf time_sec
+	call write_time
+	goto release
+week_up
+	movff time_week, output1
+	movlw 0x07
+	subwf output1
+	bz no_increase
+	incf time_week
+	call write_time
+no_increase
+	goto release
+	
 	
 character3	
 	movlw   0x33
@@ -263,11 +352,72 @@ character3
 	bra     release
 	
 character4	
-	movlw   0x34
-	movwf   output2
-	movlw	1	; output message to LCD
-	lfsr	FSR2, output2
-	call	LCD_Write_Message
+	btfsc   mode_counter, 0
+	bra	left
+	bra	other
+left	
+	movff set_position, output1
+	movlw 0x00
+	subwf output1
+	bz other
+	
+	movff set_position, output1
+	movlw 0x01
+	subwf output1
+	bz position_n1
+	
+	movff set_position, output1
+	movlw 0x02
+	subwf output1
+	bz position_n2
+	
+	movff set_position, output1
+	movlw 0x03
+	subwf output1
+	bz position_n3
+	
+position_n1	
+	decf set_position
+	call Line_set_2
+	call chevron
+	call chevron
+	call spaces1
+	call spaces1
+	call spaces1
+	bra other
+	
+position_n2
+	decf set_position
+	call Line_set_2
+	call spaces1
+	call spaces1
+	call spaces1
+	call chevron
+	call chevron
+	call spaces1
+	call spaces1
+	call spaces1
+	bra other
+	
+position_n3
+	decf set_position
+	call Line_set_2
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call chevron
+	call chevron
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	bra other
+
+other
 	bra     release
 	
 character5	
@@ -278,13 +428,65 @@ character5
 	call	LCD_Write_Message
 	bra     release
 	
-character6	
-	movlw   0x36
-	movwf   output2
-	movlw	1	; output message to LCD
-	lfsr	FSR2, output2
-	call	LCD_Write_Message
-	bra     release
+character6
+	btfsc   mode_counter, 0
+	bra	right
+	bra	other
+right	
+	movff set_position, output1
+	movlw 0x03
+	subwf output1
+	bz other
+position_1	
+	movff set_position, output1
+	movlw 0x01
+	subwf output1
+	bz position_2
+	
+	movff set_position, output1
+	movlw 0x02
+	subwf output1
+	bz position_3
+	
+	incf set_position
+	call Line_set_2
+	call spaces1
+	call spaces1
+	call spaces1
+	call chevron
+	call chevron
+	bra other
+	
+position_2	
+	incf set_position
+	call Line_set_2
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call chevron
+	call chevron
+	bra other
+	
+position_3	
+	incf set_position
+	call Line_set_2
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call spaces1
+	call chevron
+	call chevron
+	call chevron
+	bra other
 	
 character7	
 	movlw   0x37
@@ -295,12 +497,63 @@ character7
 	bra     release
 	
 character8	
-	movlw   0x38
-	movwf   output2
-	movlw	1	; output message to LCD
-	lfsr	FSR2, output2
-	call	LCD_Write_Message
-	bra     release
+	btfsc   mode_counter, 0
+	bra	down
+	bra	other
+down
+	movff set_position, output1
+	movlw 0x00
+	subwf output1
+	bz hour_down
+	
+	movff set_position, output1
+	movlw 0x01
+	subwf output1
+	bz minute_down
+	
+	movff set_position, output1
+	movlw 0x02
+	subwf output1
+	bz second_down
+	
+	movff set_position, output1
+	movlw 0x03
+	subwf output1
+	bz week_down
+	
+hour_down
+	movff time_hour, output1
+	movlw 0x00
+	subwf output1
+	bz no_decrease
+	decf time_hour
+	call write_time
+	goto release
+minute_down
+	movff time_min, output1
+	movlw 0x00
+	subwf output1
+	bz no_decrease
+	decf time_min
+	call write_time
+	goto release
+second_down
+	movff time_sec, output1
+	movlw 0x00
+	subwf output1
+	bz no_decrease
+	decf time_sec
+	call write_time
+	goto release
+week_down
+	movff time_week, output1
+	movlw 0x01
+	subwf output1
+	bz no_decrease
+	decf time_week
+	call write_time
+no_decrease
+	goto release
 	
 character9	
 	movlw   0x39
@@ -351,19 +604,36 @@ characterD
 	bra     release
 	
 characterE	
-	movlw   0x45
-	movwf   output2
-	movlw	1	; output message to LCD
-	lfsr	FSR2, output2
-	call	LCD_Write_Message
+	btfsc	mode_counter, 0
+	bra     release
+	btfss   mode_counter, 1
+	bra	set_date
+	bra	start_date
+set_date
+	bsf	mode_counter, 1
+	call	Line_set_1
+	call    chevron_v
+	call	chevron_v
+	movlw	0x00
+	movwf   set_position
+	bra	release
+start_date
+	bcf     mode_counter, 1
 	bra     release
 	
 characterF
+	btfsc	mode_counter, 1
+	bra	release
 	btfss   mode_counter, 0
 	bra	set_time
 	bra	start_time
 set_time
 	bsf	mode_counter, 0
+	call	Line_set_2
+	call    chevron
+	call	chevron
+	movlw	0x00
+	movwf   set_position
 	bra	release
 start_time
 	bcf     mode_counter, 0
@@ -413,6 +683,29 @@ set_line2
     call     Line_set_2
     bra      finish2
     
+chevron
+    movlw   0x5e
+    movwf   output1
+    movlw   1	; output message to LCD
+    lfsr    FSR2, output1
+    call    LCD_Write_Message
+    return
+    
+chevron_v
+    movlw   0x76
+    movwf   output1
+    movlw   1	; output message to LCD
+    lfsr    FSR2, output1
+    call    LCD_Write_Message
+    return
+    
+spaces1
+	movlw   0x20
+	movwf   output1
+	movlw	1	; output message to LCD
+	lfsr	FSR2, output1
+	call	LCD_Write_Message
+	return
     end
 
 
